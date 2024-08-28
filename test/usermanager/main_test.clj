2d2e40abf7283845bd9a72feb3c1868848364e1c
Aditya Athalye <aditya.athalye@gmail.com>
2024-08-28 16:37:54 +0530

    Init barebones app with test for echo handler

---
+++ test/usermanager/main_test.clj
@@ -0,0 +1,18 @@
+(ns usermanager.main-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [clj-http.client :as http]
+            [usermanager.main :as um]))
+
+(deftest a-simple-server-test
+  (testing "A simple server test."
+    (let [server (um/-main)
+          base-uri (.toString (.getURI server))]
+      (is (= {:status 200
+              :headers {"Content-Type" "text/plain;charset=utf-8"}
+              :body "echoing METHOD :get for PATH /"}
+             (-> (http/get base-uri)
+                 (select-keys [:status :body :headers])
+                 (update :headers (fn [{:strs [Content-Type]}]
+                                    {"Content-Type" Content-Type}))))
+          "Server echoes back request information in body.")
+      (.stop server))))
