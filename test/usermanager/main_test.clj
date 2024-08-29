f100c914e68272974326dc9037c952191918f550
Aditya Athalye <aditya.athalye@gmail.com>
2024-08-29 15:41:52 +0530

    Move test utilities to dedicated namespace for reuse

--- test/usermanager/main_test.clj
+++ test/usermanager/main_test.clj
@@ -1,30 +1,15 @@
 (ns usermanager.main-test
-  (:require [clojure.test :refer [deftest is testing] :as t]
-            [clj-http.client :as http]
-            [usermanager.main :as um]
-            [usermanager.system.core :refer :all])
-  (:import (java.net ServerSocket)))
+  (:require
+   [clj-http.client :as http]
+   [clojure.test :as t :refer [deftest is testing]]
+   [usermanager.system.core :as system]
+   [usermanager.test-utilities :as tu]))
 
-(defn get-free-port!
-  []
-  (with-open [socket (ServerSocket. 0)]
-    (.getLocalPort socket)))
-
-(defn setup-teardown-server!
-  [f]
-  (set-config! ::server {:port (get-free-port!)
-                         :join? false})
-  (println (format "\nRunning test with server config: %s\n"
-                   (get-config ::server)))
-  (start-server! (um/wrap-router um/echo-handler) ::server)
-  (f)
-  (stop-server! ::server))
-
-(t/use-fixtures :once setup-teardown-server!)
+(t/use-fixtures :once (partial tu/setup-teardown-server! ::server))
 
 (deftest a-simple-server-test
   (testing "A simple server test."
-    (let [base-uri (.toString (.getURI (get-state ::server)))]
+    (let [base-uri (.toString (.getURI (system/get-state ::server)))]
       (is (= {:status 200
               :headers {"Content-Type" "text/plain;charset=utf-8"}
               :body "echoing METHOD :get for PATH /"}
--- test/usermanager/router/core_test.clj
+++ test/usermanager/router/core_test.clj
@@ -1,47 +1,37 @@
 (ns usermanager.router.core-test
-  (:require [clojure.test :refer [deftest is are testing]]
-            [usermanager.router.core :as urc]
-            [usermanager.main :as um]))
-
-(defn- echo-response
-  [method path]
-  {:status 200
-   :headers {"Content-Type" "text/plain;charset=utf-8"}
-   :body (format "echoing METHOD %s for PATH %s"
-                 method path)})
-
-(def not-found-response
-  {:status 404
-   :headers {}
-   :body "Not Found."})
+  (:require
+   [clojure.test :refer [are deftest testing]]
+   [usermanager.main :as um]
+   [usermanager.router.core :as urc]
+   [usermanager.test-utilities :as tu]))
 
 (deftest route-matching-test
   (testing "Only allowed route path patterns match."
     (are [route-call response] (= route-call response)
 
       (urc/router um/echo-handler {:request-method :get :uri "/"})
-      (echo-response :get "/")
+      (tu/echo-response :get "/")
 
       (urc/router um/echo-handler {:request-method :post :uri "/"})
-      not-found-response
+      tu/not-found-response
 
       (urc/router um/echo-handler {:request-method :post :uri "/does/not/exist"})
-      not-found-response
+      tu/not-found-response
 
       (urc/router um/echo-handler {:request-method :delete :uri "/user/delete/42"})
-      (echo-response :delete "/user/delete/42")
+      (tu/echo-response :delete "/user/delete/42")
 
       (urc/router um/echo-handler {:request-method :post :uri "/user/delete/42"})
-      not-found-response
+      tu/not-found-response
 
       (urc/router um/echo-handler {:request-method :delete :uri "/user/delete/NAN"})
-      not-found-response
+      tu/not-found-response
 
       (urc/router um/echo-handler {:request-method :get :uri "/user/form/42"})
-      (echo-response :get "/user/form/42")
+      (tu/echo-response :get "/user/form/42")
 
       (urc/router um/echo-handler {:request-method :post :uri "/user/form/42"})
-      not-found-response
+      tu/not-found-response
 
       (urc/router um/echo-handler {:request-method :form :uri "/user/form/NAN"})
-      not-found-response)))
+      tu/not-found-response)))
