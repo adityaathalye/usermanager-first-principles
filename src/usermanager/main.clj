3227919b056624fed04a9b0167f64c623d9d7116
Aditya Athalye <aditya.athalye@gmail.com>
2024-08-31 18:34:19 +0530

    Remove circular dependency between system and middleware
    Accidentally introduced in the earlier commit.

--- src/usermanager/main.clj
+++ src/usermanager/main.clj
@@ -2,17 +2,23 @@
   (:gen-class)
   (:require
    [usermanager.router.core :as router]
-   [usermanager.system.core :as system]))
+   [usermanager.system.core :as system]
+   [usermanager.http.middleware :as middleware]))
+
+(def middleware-stack [middleware/wrap-db])
 
 (defn wrap-router
-  [router & middleware-key]
-  (apply system/start-middleware-stack! middleware-key)
-  (fn [request]
-    (let [request-handler (router request)
-          app-handler (apply system/wrap-middleware
-                             request-handler
-                             middleware-key)]
-      (app-handler request))))
+  ([router]
+   (wrap-router router ::system/middleware))
+  ([router middleware-key]
+   (system/set-config! middleware-key middleware-stack)
+   (system/start-middleware-stack! middleware-key)
+   (fn [request]
+     (let [request-handler (router request)
+           app-handler (system/wrap-middleware
+                        request-handler
+                        middleware-key)]
+       (app-handler request)))))
 
 (defn -main
   [& _args]
